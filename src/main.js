14ba875099846881bc6d426e77f98ba8496ff089
ishmal <ishmalius@gmail.com>
2018-05-31 22:20:28 -0500

    add reloader

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
+const reload = require("express-reload");
+
+
 const tiny = require("./tiny");
 const index = require("./index");
 
 const app = express();
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const filePath = path.join(__dirname, "index.js");
+
+app.use(reload(filePath));
 
 /**
  * Routes
@@ -14,6 +22,6 @@ app.get("/", index);
 app.get("/tiny/:url", tiny.get);
 app.post("/tiny", urlencodedParser, tiny.post);
 
-app.listen(8080, () => {
-  console.log("listening on 8080");
+app.listen(3000, () => {
+  console.log("listening on 3000");
 });
