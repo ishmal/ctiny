9021fc64a3697ddac74c378453d1da61e9f1c1e8
ishmal <ishmal@github.com>
2018-06-02 17:11:57 -0500

    validation

--- public/client.js
+++ public/client.js
@@ -1,3 +1,31 @@
+		function error(msg) {
+			let fld = document.getElementById("errorfield");
+			fld.textContent = msg;
+			setTimeout(function() {
+				fld.textContent = "";
+			}, 3000);
+		}
+
+		function validateShortUrl(str) {
+			let rgx = /^[A-Za-z]{1,5}$/;
+			str = str.trim();
+			let res = rgx.test(str);
+			if (!res) {
+				error("invalid Tiny URL.  1-5 letters A-Z or a-z");
+			}
+			return res;
+		}
+		
+		function validateUrl(str) {
+			let rgx = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/i;
+			str = str.trim();
+			let res = rgx.test(str);
+			if (!res) {
+				error("invalid URL");
+			}
+			return res;
+		}
+		
 		function copyToClipBoard() {
 			var copyText = document.getElementById("shorturl");
 			var copyAlert = document.getElementById("copyalert");
@@ -15,6 +43,10 @@
 			let encodeField = document.getElementById("encodefield");
 			let decodeField = document.getElementById("decodefield");
 			let plaintext = encodeField.value;
+			let res = validateUrl(plaintext);
+			if (!res) {
+				return;
+			}
 			decodeField.value = "processing";
 			let opts = {
 				method: 'POST',
@@ -39,6 +71,10 @@
 			let encodeField = document.getElementById("encodefield");
 			let decodeField = document.getElementById("decodefield");
 			let encoded = decodeField.value;
+			let res = validateShortUrl(encoded);
+			if (!res) {
+				return;
+			}
 			encodeField.value = "processing";
 			let tinyWindow = window.open("", "ctiny");
 			fetch("/tiny/" + encoded)
