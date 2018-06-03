7bbc520e8f0305b6d2dc3cfc171b3462d0b7855b
ishmal <ishmal@github.com>
2018-06-03 12:34:48 -0500

    improve styles

--- public/client.js
+++ public/client.js
@@ -1,110 +1,146 @@
-		function error(msg) {
-			let fld = document.getElementById("errorfield");
-			fld.textContent = msg;
-			setTimeout(function() {
-				fld.textContent = "";
-			}, 3000);
-		}
+	
+function alertTemplate(type, msg) {
+	let div = document.createElement("div");
+	div.classList.add("alert", "alert-" + type);
+	div.textContent = msg;
+	return div;
+}
 
-		function validateShortUrl(str) {
-			let rgx = /^[A-Za-z]{1,5}$/;
-			str = str.trim();
-			let res = rgx.test(str);
-			if (!res) {
-				error("invalid Tiny URL.  1-5 letters A-Z or a-z");
+/**
+ * Browser client for the CTiny backend
+ */
+class TinyClient {
+
+	constructor() {
+		this.encodeField = document.getElementById("encodeField");
+		this.decodeField = document.getElementById("decodeField");
+		this.encodeBtn = document.getElementById("encodeBtn");
+		this.decodeBtn = document.getElementById("decodeBtn");
+		this.alertsAnchor = document.getElementById("alerts");
+
+		this.encodeBtn.addEventListener("click", () => {
+			this.encodeUrl();
+		});
+		this.decodeBtn.addEventListener("click", () => {
+			this.decodeUrl();
+		});
+		this.encodeField.addEventListener("keyup", (evt) => {
+			if (evt.keyCode === 13) {
+				this.encodeUrl();
+				evt.preventDefault();
 			}
-			return res;
-		}
-		
-		function validateUrl(str) {
-			let rgx = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/i;
-			str = str.trim();
-			let res = rgx.test(str);
-			if (!res) {
-				error("invalid URL");
+		});
+		this.decodeField.addEventListener("keyup", (evt) => {
+			if (evt.keyCode === 13) {
+				this.decodeUrl();
+				evt.preventDefault();
 			}
-			return res;
-		}
-		
-		function copyToClipBoard() {
-			var copyText = document.getElementById("shorturl");
-			var copyAlert = document.getElementById("copyalert");
-			var range = document.createRange();
-			range.selectNode(copyText);
-			var result = document.execCommand("copy");
-			var msg = result ? "copied!" : "not copied!";
-			copyAlert.textContent = msg;
-			setTimeout(function () {
-				copyAlert.textContent = "";
-			}, 2000);
-		}
+		});
+		//$('.alert').alert()
+	}
 
-		function encodeUrl() {
-			let encodeField = document.getElementById("encodefield");
-			let decodeField = document.getElementById("decodefield");
-			let plaintext = encodeField.value;
-			/*
-			let res = validateUrl(plaintext);
-			if (!res) {
-				return;
+	error(msg) {
+		let anchor = this.alertsAnchor;
+		let div = document.createElement("div");
+		div.classList.add("alert", "alert-danger");
+		div.textContent = msg;
+		anchor.appendChild(div);
+		setTimeout(function() {
+			anchor.removeChild(div);
+		}, 2000);
+	}
+
+	alert(msg) {
+		let anchor = this.alertsAnchor;
+		let div = document.createElement("div");
+		div.classList.add("alert", "alert-success");
+		div.textContent = msg;
+		anchor.appendChild(div);
+		setTimeout(function() {
+			anchor.removeChild(div);
+		}, 2000);
+	}
+
+	validateShortUrl(str) {
+		let rgx = /^[A-Za-z]{1,5}$/;
+		str = str.trim();
+		let res = rgx.test(str);
+		if (!res) {
+			this.error("A Tiny URL is only 1-5 letters A-Z or a-z");
+		}
+		return res;
+	}
+	
+	validateUrl(str) {
+		let rgx = /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/i;
+		str = str.trim();
+		let res = rgx.test(str);
+		if (!res) {
+			this.error("invalid URL");
+		}
+		return res;
+	}
+	
+	copyToClipBoard(elem) {
+		document.createRange().selectNode(elem);
+		let result = document.execCommand("copy");
+		if (result) {
+			this.alert("copied to clipboard");
+		} else {
+			this.error("not copied to clipboard");
+		}
+	}
+
+	encodeUrl() {
+		let plaintext = this.encodeField.value;
+		/*
+		let res = validateUrl(plaintext);
+		if (!res) {
+			return;
+		}
+		*/
+		this.decodeField.value = "processing";
+		let opts = {
+			method: 'POST',
+			body: JSON.stringify({ url: plaintext }),
+			headers: {
+				'Content-Type': 'application/json'
 			}
-			*/
-			decodeField.value = "processing";
-			let opts = {
-				method: 'POST',
-				body: JSON.stringify({ url: plaintext }),
-				headers: {
-					'Content-Type': 'application/json'
-				}
-			};
-			fetch("/tiny", opts)
-			.then(res => {
-				return res.text();
-			})
-			.then(url => {
-				decodeField.value = url;
-			})
-			.catch(err => {
-				console.log(err);
-			});
+		};
+		fetch("/tiny", opts)
+		.then(res => {
+			return res.text();
+		})
+		.then(url => {
+			this.decodeField.value = url;
+		})
+		.catch(err => {
+			console.log(err);
+		});
+	}
+
+	decodeUrl() {
+		let encoded = this.decodeField.value;
+		let res = this.validateShortUrl(encoded);
+		if (!res) {
+			return;
 		}
-
-		function decodeUrl() {
-			let encodeField = document.getElementById("encodefield");
-			let decodeField = document.getElementById("decodefield");
-			let encoded = decodeField.value;
-			let res = validateShortUrl(encoded);
-			if (!res) {
-				return;
-			}
-			encodeField.value = "processing";
-			let tinyWindow = window.open("", "ctiny");
-			fetch("/tiny/" + encoded)
-			.then(res => {
-				return res.text();
-			})
-			.then(url => {
-				encodeField.value = url;
-				tinyWindow.location = url;
-			});
-
-		}
-
-		document.addEventListener("DOMContentLoaded", () => {
-			let encodeField = document.getElementById("encodefield");
-			let decodeField = document.getElementById("decodefield");
-			encodeField.addEventListener("keyup", (evt) => {
-				if (evt.keyCode === 13) {
-					encodeUrl();
-					evt.preventDefault();
-				}
-			});
-			decodeField.addEventListener("keyup", (evt) => {
-				if (evt.keyCode === 13) {
-					decodeUrl();
-					evt.preventDefault();
-				}
-			});
+		this.encodeField.value = "processing";
+		let tinyWindow = window.open("", "ctiny");
+		fetch("/tiny/" + encoded)
+		.then(res => {
+			return res.text();
+		})
+		.then(url => {
+			this.encodeField.value = url;
+			tinyWindow.location = url;
 		});
 
+	}
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	let client = new TinyClient();
+});
 
