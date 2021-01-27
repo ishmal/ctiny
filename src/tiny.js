f3a0ecfc001b160d0a37cdf38b90160d002dc69d
ishmal <ishmal@github.com>
2021-01-27 12:25:23 -0600

    updated

--- src/tiny.js
+++ src/tiny.js
@@ -31,40 +31,61 @@ function getTime() {
  * Simple service that creates and manages "tiny" URLs
  */
 class Tiny {
+
+
 	constructor() {
+		/**
+		 * For each encoded url, we have a single record, but
+		 * 3 tables that reference the record.
+		 */
+		// Short -> rec
+		this.forward = {};
+		// URL -> rec
+		this.reverse = {};
+		// allocation table,  int -> rec
 		this.table = {};
-		this.get = (req, res) => {
-			return this.handleGet(req, res);
-		};
-		this.post = (req, res) => {
-			return this.handlePost(req, res);
-		};
+
+		// expose these to container as functions
+		this.get = this.handleGet.bind(this);
+		this.post = this.handlePost.bind(this);
+
 		this.gcStart();
+
+		// one-off cache
 		this.lastUrl = "";
 		this.lastShort = null;
 	}
 
+	/**
+	 * Clear out expired records from the 3 tables
+	 */
 	gc() {
 		const now = getTime();
 		let nrDeleted = 0;
-		function doTable(table) {
-			Object.entries(this.table).forEach(([k, v]) => {
-				if (v.timeout < now) {
-					delete table[k];
-					nrDeleted++;
-				}
-			});	
-		}
-		doTable(this.table);
-		console.log(`Keys freed: ${nrDeleted}`);
+		const table = this.table;
+		Object.entries(table).forEach(([k, v]) => {
+			if (v && v.timeout < now) {
+				delete this.forward[v.encoded];
+				delete this.reverse[v.plain];
+				delete table[k];
+				nrDeleted++;
+			}
+		});	
+		console.log(`Records freed: ${nrDeleted}`);
 	}
 
+	/**
+	 * Start the gc background process
+	 */
 	gcStart() {
 		this.gcInterval = setInterval(() => {
 			this.gc();
 		}, GC_PERIOD);
 	}
 
+	/**
+	 * Stop the gc background process
+	 */
 	gcStop() {
 		if (this.gcInterval) {
 			clearInterval(this.gcInterval);
@@ -72,6 +93,11 @@ class Tiny {
 		}
 	}
 
+	/**
+	 * Encode an integer as digits
+	 * @param {number} nr integer to encode
+	 * @return {string} string of digits
+	 */
 	encode(nr) {
 		let code = "";
 		do {
@@ -82,6 +108,11 @@ class Tiny {
 		return code;
 	}
 
+	/**
+	 * Convert a string if digits to an integer
+	 * @param {string} str string of digits
+	 * @return {number} integer 
+	 */
 	decode(str) {
 		str = str.toLowerCase();
 		let val = 0;
@@ -91,12 +122,17 @@ class Tiny {
 				return -1;
 			}
 			const v = DIGITS_REV[c];
-			val = val * 26 + v;
+			val = val * DIGITS_LEN + v;
 		}
 		return val;
 	}
 
-	findKey() {
+	/**
+	 * Find the lowest available slot (and thus the shortest 
+	 * encoded url) in the allocation table
+	 * @return {number} the index of the lowest slot, else null
+	 */
+	findAvailableSlot() {
 		const table = this.table;
 		//search ordinally in spite of being sparse
 		for (let i = 0; i < 10000000 ; i++) {
@@ -107,66 +143,97 @@ class Tiny {
 		return null;
 	}
 
+	/**
+	 * Create a new shortened url from a given long one. 
+	 * In order:
+	 * 1)  Check the cache for the url
+	 * 2)  Check the table for the url
+	 * 3)  Make a new one
+	 * @param {string} url the url to encode
+	 * @return {string} the shortened url
+	 */
 	create(url) {
 		// check cache first
 		if (url === this.lastUrl) {
 			return this.lastShort;
 		}
-		const table = this.table;
 
 		/**
 		 * First see if the url is already shortened.  If
 		 * so, simply update it and return the existing short code.
 		 */
-		Object.entries(table).forEach(([k, v]) => {
-			if (v.url === url) {
-				v.timeout = getTime() + TIMEOUT;
-				const encoded = this.encode(k);
-				//update cache
-				this.lastUrl = url;
-				this.lastShort = encoded;
-				return encoded;
-			}
-		});
+		const rec = this.reverse[url];
+
+		if (rec) {
+			rec.timeout = getTime() + TIMEOUT;
+			const encoded = rec.url;
+			this.lastUrl = url;
+			this.lastShort = encoded;
+			return encoded;
+		}
 
 		/**
 		 * Otherwise find the lowest key in the table, and make
 		 * a new record.
 		 */
-		const index = this.findKey();
+		const index = this.findAvailableSlot();
 		if (index !== null) {
 			const encoded = this.encode(index);
-			table[index] = {
+			const rec = {
 				timeout: getTime() + TIMEOUT,
-				url: url
+				url: url,
+				encoded: encoded
 			};
+			this.table[index] = rec;
+			this.forward[encoded] = rec;
+			this.reverse[url] = rec;
 			// update cache
 			this.lastUrl = url;
 			this.lastShort = encoded;
 			return encoded;
 		} else {
-			// what?
+			return null;
 		}
 	}
 
+	/**
+	 * Convert a short url to the original full orl
+	 * @param {string} shortUrl 
+	 * @return {string} the plain url if found, else null
+	 */
 	fetch(shortUrl) {
 		const table = this.table;
-		const index = this.decode(shortUrl);
-		const res = table[index] || { url: null };
-		const url = res.url;
-		return url;
+		const rec = this.forward[shortUrl];
+		if (rec) {
+			rec.timeout = getTime() + TIMEOUT;
+			this.lastUrl = rec.url;
+			this.lastShort = rec.encoded;
+			return rec.url;
+		} else {
+			return null;
+		}
 	}
 
+	/**
+	 * Handle an HTTP 'get' for a full url
+	 * @param {object} req http request
+	 * @param {object} res http response 
+	 */
 	handleGet(req, res) {
 		const shortUrl = req.params.url;
-		const fullUrl = this.fetch(shortUrl);
+		const fullUrl = this.fetch(shortUrl) || "Not found";
 		res.type("text/plain");
 		res.send(fullUrl);	
 	}
 
+	/**
+	 * Handle an HTTP 'post' to create (or fetch) a short URL
+	 * @param {object} req http request
+	 * @param {object} res http response 
+	 */
 	handlePost(req, res) {
 		const url = req.body.url;
-		const shortUrl = this.create(url);
+		const shortUrl = this.create(url) || "Not found";
 		res.type("text/plain");
 		res.send(shortUrl);	
 	}
